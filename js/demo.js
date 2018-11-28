2fb120d0257cdca91868420886468adad7dd6b4e
Asgar Mirza <asgarmirza5@gmail.com>
2018-11-28 17:50:51 -0600

    First website upload

---
+++ js/demo.js
@@ -0,0 +1,11 @@
+<script>
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "Please contact Kyung Kim (PI) at <u>kkim2@niu.edu</u> for the invitation code required in the account registration.";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+</script>
